d460f3c57fe73ec09c85c8cad4e666295b3755f6
michelJesus <m.micheljesus@gmail.com>
2015-04-08 02:10:57 -0300

    Versao 08 04 2015

---
+++ LayoutcomASPMVCTemplate/LayoutcomASPMVCTemplate/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutcomASPMVCTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Examples()
+        {
+            return View();
+        }
+
+        public ActionResult APage()
+        {
+            return View();
+        }
+
+        public ActionResult AnotherPage()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
