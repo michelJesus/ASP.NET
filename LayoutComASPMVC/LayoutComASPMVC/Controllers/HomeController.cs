efe7b54d0e4053ec98f5eefeed0bc98d4d57ef79
michelJesus <m.micheljesus@gmail.com>
2015-04-09 15:32:44 -0300

    v09042015

---
+++ LayoutComASPMVC/LayoutComASPMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutComASPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        public ActionResult Listagem()
+        {
+            return View();
+        }
+    }
+}
