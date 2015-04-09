73ec5be994c916234cfd3449034ff6e88f0ef992
michelJesus <m.micheljesus@gmail.com>
2015-04-09 15:22:13 -0300

    v09042015

---
+++ ExemploWebForms/ExemploWebForms/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExemploWebForms.Startup))]
+namespace ExemploWebForms
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
